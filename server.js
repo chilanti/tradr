74ebd0b110c4339c7649f089bcf90365b0dc48e7
mchilant <mchilant@micheles-mbp.rchland.ibm.com>
2019-03-07 10:05:59 -0600

    Typo.

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ var tokenGen = require('./jwt/token')
 var passport = require('passport');
 
 //Added to handle self-signed certs
-var https = require('https').globalAgent.options.rejectUnauthorized = false;
+require('https').globalAgent.options.rejectUnauthorized = false;
 //End addition
 
 console.log(process.env);
